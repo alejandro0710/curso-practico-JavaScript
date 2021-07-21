4050840585e22645d4045b15f470fab7c49419e4
Alejandro Ramos <alejandro.ramosjarf@gmail.com>
2021-07-21 18:57:53 -0500

    Terminamos codigo de JS, implementamos html y css

--- figuras.js
+++ figuras.js
@@ -1,5 +1,5 @@
 
-console.log("Hola mundo, bienvenido a mi calculadora de geometría de área y perimetro");
+console.log("Hola mundo, bienvenido a mi calculadora de geometría de área y perimetro (cuadrado,triangulo y circulo)");
 
 // Codigo del Cuadrado
 console.group("CUADRADO");
@@ -47,6 +47,68 @@ function areaCirculo(radio){
 
 console.groupEnd();
 
+//Funciones conectadas con HTML
 
+//CUADRADO
+function CalcularPerimetroCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+    const valorUsuario = input.value;
 
+    const Perimetro = perimetroCuadrado(valorUsuario)
+    alert("El perimetro de tu Cuadrado es: " + Perimetro);
+}
 
+function CalcularAreaCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+    const valorUsuario = input.value;
+
+    const area = areaCuadrado(valorUsuario)
+    alert("El area de tu Cuadrado es: " + area);
+}
+
+//TRIANGULO
+function CalcularPerimetroTriangulo(){
+    const Lado1 = document.getElementById("inputLado1");
+    const Lado2 = document.getElementById("inputLado2");
+    const Base = document.getElementById("inputBase");
+    
+    const valorUsuarioLado1 = Number(Lado1.value);
+    const valorUsuarioLado2 = Number(Lado2.value);
+    const valorUsuarioBase = Number(Base.value);
+
+    const perimetro = perimetroTriangulo(valorUsuarioLado1, valorUsuarioLado2, valorUsuarioBase)
+    alert("El perimetro de tu Triangulo es: " + perimetro);
+}
+
+function CalcularAreaTriangulo(){
+    const Lado1 = document.getElementById("inputLado1");
+    const Lado2 = document.getElementById("inputLado2");
+    const Base = document.getElementById("inputBase");
+    
+    const valorUsuarioLado1 = Number(Lado1.value);
+    const valorUsuarioLado2 = Number(Lado2.value);
+    const valorUsuarioBase = Number(Base.value);
+
+    const perimetro = perimetroTriangulo(valorUsuarioLado1, valorUsuarioLado2, valorUsuarioBase)
+    const Sperimetro = perimetro / 2;
+    const area = Math.sqrt(Sperimetro * (Sperimetro - valorUsuarioLado1) * (Sperimetro - valorUsuarioLado2) * (Sperimetro - valorUsuarioBase));
+
+    alert("El area de tu Triangulo es: " + area);
+}
+
+//CIRCULO
+function CalcularPerimetroCirculo(){
+    const radio = document.getElementById("inputRadio");
+    const valorUsuarioRadio = radio.value;
+
+    const perimetro = perimetroCirculo(valorUsuarioRadio);
+    alert("Este es el perimetro de tu Circulo: " + perimetro);
+}
+
+function CalcularAreaCirculo(){
+    const radio = document.getElementById("inputRadio");
+    const valorUsuarioRadio = radio.value;
+
+    const area = areaCirculo(valorUsuarioRadio);
+    alert("Este es el area de tu Circulo: " + area);
+}
